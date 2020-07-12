2232cd007935e6e7bcd281ede14a5d428da4cb04
Lazar Sormaz <shormaz@outlook.com>
2020-07-12 18:59:32 +0200

    * new component added for image-upload which is a part of create-ad component

---
+++ src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, EventEmitter, Output } from '@angular/core';
+import { AdsService } from '../@core/services/ads.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-image-upload',
+  templateUrl: './image-upload.component.html',
+  styleUrls: ['./image-upload.component.scss']
+})
+export class ImageUploadComponent implements OnInit {
+
+
+  @Output() onImagePicked: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild("fileUpload", {static: false}) fileUpload: ElementRef;
+
+  files  = [];
+  pending: boolean = false;
+  status: string = 'init';
+  imagesList = []
+  imagePreview;
+  imagesPreview = [];
+  spinner: boolean;
+
+
+  constructor(private imageService: AdsService){}
+
+  ngOnInit() {
+
+  }
+
+  public pickImages() {
+    this.onImagePicked.emit(this.imagesList);
+}
+
+
+
+    uploadFile(formData) {
+      this.imageService.uploadImageInStorage(formData).subscribe(
+        response => {
+          this.imagesList = [...this.imagesList, ...response]
+          this.pickImages()
+        })
+
+    }
+
+
+  //   private uploadFiles() {
+  //     this.fileUpload.nativeElement.value = '';
+  //     this.imagesList.forEach(file => {
+  //       this.uploadFile(file);
+  //     });
+  // }
+
+  onClick() {
+    const fileUpload = this.fileUpload.nativeElement;
+
+    fileUpload.onchange = () => {
+    for (let index = 0; index < fileUpload.files.length; index++)
+    {
+     const file = fileUpload.files[index];
+     const formData = new FormData();
+      formData.append('file', file);
+     this.uploadFile(formData);
+     this.files.push({ data: file, inProgress: false, progress: 0});
+    }
+      // this.uploadFiles();
+
+    };
+    fileUpload.click();
+}
+
+}
