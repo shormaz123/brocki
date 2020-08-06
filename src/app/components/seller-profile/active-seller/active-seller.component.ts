aeb4f44cfe8c734da2b088c84720e74896755d9a
JovovicNovica <50376023+JovovicNovica@users.noreply.github.com>
2020-08-06 20:21:49 +0200

    feat(seller, seller-active, seller-sold, seller-aboutUs)

--- src/app/components/seller-profile/active-seller/active-seller.component.ts
+++ src/app/components/seller-profile/active-seller/active-seller.component.ts
@@ -1,28 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from "@angular/core";
+import { Ads } from "../../../shared/models/ads.model";
 
 @Component({
-  selector: 'app-active-seller',
-  templateUrl: './active-seller.component.html',
-  styleUrls: ['./active-seller.component.scss']
+  selector: "app-active-seller",
+  templateUrl: "./active-seller.component.html",
+  styleUrls: ["./active-seller.component.scss"],
 })
-export class ActiveSellerComponent implements OnInit {
+export class ActiveSellerComponent implements OnChanges {
+  @Input() activeProducts: Ads;
+  @Input() ads: boolean;
+  constructor() {}
 
-  ads = [{
-    title: 'Toyota',
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    price: '2900 chf',
-    days: '5 days ago'
-  },
-{
-    title: 'Jewelery set',
-    text: 'Another text description',
-    price: '144 chf',
-    days: '14 days ago'
-}]
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnChanges() {}
 }
