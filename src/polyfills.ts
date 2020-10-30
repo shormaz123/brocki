f57fd5c7bed9c772d0e1877fffbb1e12a8f15236
savkevip <savkevip@gmail.com>
2020-10-30 20:48:09 +0100

    process fix

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,11 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/*
+
+ */
+
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
