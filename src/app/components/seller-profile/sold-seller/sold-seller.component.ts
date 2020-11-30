6511931d48d57c35e9e824579371476bd7adc724
Lazar Sormaz <shormaz@outlook.com>
2020-11-30 14:48:35 +0100

    *button hidden if there are no ads logic fixed

--- src/app/components/seller-profile/sold-seller/sold-seller.component.ts
+++ src/app/components/seller-profile/sold-seller/sold-seller.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
-import { Ads } from '../../../shared/models/ads.model';
+import { Component, OnChanges, Input, OnInit } from '@angular/core';
 import { AdsService } from '../../../@core/services/ads.service';
 
 @Component({
@@ -7,8 +6,8 @@ import { AdsService } from '../../../@core/services/ads.service';
   templateUrl: './sold-seller.component.html',
   styleUrls: ['./sold-seller.component.scss'],
 })
-export class SoldSellerComponent implements OnChanges {
-  @Input() soldProducts: Ads;
+export class SoldSellerComponent implements OnChanges, OnInit {
+  @Input() soldProducts: any;
   @Input() ads: boolean;
 
   paginationNumber: number = 1;
@@ -19,7 +18,16 @@ export class SoldSellerComponent implements OnChanges {
 
   constructor(private adsService: AdsService) {}
 
-  ngOnChanges() {}
+  ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.ads === false) {
+      this.disableButton = false;
+    } else {
+      this.disableButton = true;
+    }
+  }
 
   increaseShow() {
     (this.paginationNumber += 1), (this.status = 'SOLD'), (this.userId = 265);
@@ -33,6 +41,7 @@ export class SoldSellerComponent implements OnChanges {
       .subscribe((response) => {
         if (response.length !== 8) {
           this.disableButton = false;
+
         }
         const soldAds = response;
         this.soldProducts[0].push(...soldAds);
