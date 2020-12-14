32698d25fd39d53a7173190c8ac706110726a664
JovovicNovica <50376023+JovovicNovica@users.noreply.github.com>
2020-12-14 21:20:01 +0100

    fix issue deleting user

--- src/app/shared/interceptors/request.interceptor.ts
+++ src/app/shared/interceptors/request.interceptor.ts
@@ -3,11 +3,19 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthConst } from '../../@core/consts/auth.const';
+import { tap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStore } from 'app/@core/services/auth.store';
 
+@Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor(public auth: AuthStore, private route: Router) {}
+
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
@@ -15,10 +23,22 @@ export class HttpRequestInterceptor implements HttpInterceptor {
     request = request.clone({
       setHeaders: {
         Authorization: `${localStorage.getItem(AuthConst.token)}`,
-        'Accept-Language': `${localStorage.getItem(AuthConst.language)}`
+        'Accept-Language': `${localStorage.getItem(AuthConst.language)}`,
       },
     });
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      tap(
+        () => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 500) {
+              this.auth.logout();
+              this.route.navigate(['/login']);
+            }
+          }
+        }
+      )
+    );
   }
 }
